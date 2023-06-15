85711bf8fc8e7b2356e7f177bbe20d666aaed54c
Rabial_Hosen <hosenrabial@gmail.com>
2023-06-15 13:21:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,33 @@
-//complete this code
-class Rectangle {}
+class Animal {
+	constructor(species){
+		this._species = species; 
+	}
+	get species(){
+		return this._species;
+	}
+	set species(species){
+		this._species = species;
+	}
+	makeSound(){
+		console.log(`The ${this._species} makes a sound`);
+	}
+}
 
-class Square extends Animal {}
+class Dog extends Animal {
+	bark(){
+		console.log("woof");
+	}
+}
+
+class Cat extends Animal {
+	purr(){
+		console.log("purr");
+	}
+}
 
 // Do not change the code below this line
-window.Rectangle = Rectangle;
-window.Square = Square;
+window.Animal = Animal;
+window.Dog = Dog;
+window.Cat = Cat;
+
+
